d5d1259872eb824dd7eb51c5af55cc2aed606800
ChouhanAkash <akashchouhan1209@gmail.com>
2025-07-07 12:41:20 +0530

    Fixed geocodecity import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 import Navbar from './components/Navbar';
@@ -7,14 +6,15 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/FleetManagerPro_Logistics-and-Transportation">
+    <div>
       <AuthProvider>
         <Navbar />
         <AppRoutes />
         <Footer/>
       </AuthProvider>
-    </BrowserRouter>
-  );
+    </div>
+  )
+    
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import { AuthProvider } from "./context/AuthContext";
 import 'leaflet/dist/leaflet.css';
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
+  <HashRouter>
+    <AuthProvider>
     <App />
   </AuthProvider>
+  </HashRouter>
 );
